d3c77d1cca6e3817bce820d6524799ecf631df18
Rob <rhlowe@gmail.com>
2021-03-24 19:40:24 -0500

    Load some posts!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [fetchStatus, setFetchStatus] = useState('ready');
+  useEffect(() => {
+    if (fetchStatus === 'ready') {
+      setFetchStatus('loading');
+
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(response => {
+          console.log(response);
+          setPosts(response);
+          setFetchStatus('done');
+        })
+        .catch(() => setFetchStatus('error'));
+    }
+  }, [fetchStatus]);
+
+  function PostList() {
+    return posts.map(post =>
+      <article>
+        <header>
+          <h2>{post.title}</h2>
+        </header>
+
+        <p>{post.body}</p>
+      </article>
+    );
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setFetchStatus('ready');
+  }
+  function ReloadButton() {
+    return (
+      <section>
+      <form onSubmit={handleSubmit}>
+        <button>
+          {fetchStatus === 'error' ? 'Retry' : 'Reload Posts'}
+        </button>
+      </form>
+      </section>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+
+      {fetchStatus === 'loading' &&
+        <section><h2>Loading...</h2></section>
+      }
+
+      {fetchStatus === 'done' &&
+        <>
+          <ReloadButton />
+          <PostList />
+        </>
+      }
+
+      {fetchStatus === 'error' &&
+        <>
+          <h2>Error 😵</h2>
+          <p>There was an error loading your posts, please try again.</p>
+          <ReloadButton />
+        </>
+      }
     </div>
   );
 }
